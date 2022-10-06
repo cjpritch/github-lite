95642a6b471cad14665f116aa1f5228f4c08f6f8
unknown <philbranch243@gmail.com>
2022-10-06 14:52:47 -0500

    seeds working good enough

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -4,81 +4,71 @@ const db = require('../config/connection');
 const { Project, User } = require('../models');
 
 db.once('open', async () => {
-  await Project.deleteMany({});
-  await User.deleteMany({});
+    await Project.deleteMany({});
+    await User.deleteMany({});
 
-  // create user data
-  const userData = [];
+    // create user data
+    const userData = [];
 
-  for (let i = 0; i < 50; i += 1) {
-    const username = faker.internet.userName();
-    const email = faker.internet.email(username);
-    const password = faker.internet.password();
+    for (let i = 0; i < 50; i += 1) {
+        const username = faker.internet.userName();
+        const email = faker.internet.email(username);
+        const password = faker.internet.password();
 
-    userData.push({ username, email, password });
-  }
+        userData.push({ username, email, password });
+    }
 
-  const createdUsers = await User.collection.insertMany(userData);
-  
-//   console.log(createdUsers);
+    const createdUsers = await User.collection.insertMany(userData);
 
-  // create friends
-  // this logic commented out because no friends in this app.  kept here because I may want to refer to it when I get around to seeding the tags below
-//   for (let i = 0; i < 100; i += 1) {
-//     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-//     const { _id: userId } = createdUsers.ops[randomUserIndex];
 
-//     let friendId = userId;
+    // create Projects
+    let createdProjects = [];
 
-//     while (friendId === userId) {
-    //   const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-//       friendId = createdUsers.ops[randomUserIndex];
-//     }
+    for (let i = 0; i < 100; i += 1) {
+        const title = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+        const description = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+        const link = faker.lorem.words(Math.round(Math.random() * 20) + 1);
 
-//     await User.updateOne({ _id: userId }, { $addToSet: { friends: friendId } });
-//   }
+        const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
+        const { username, _id: userId } = createdUsers.ops[randomUserIndex];
 
-  // create Projects
-  let createdProjects = [];
+        const createdProject = await Project.create({ title, description, link, username });
 
-  for (let i = 0; i < 100; i += 1) {
-    const title = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-    const description = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-    const link = faker.lorem.words(Math.round(Math.random() * 20) + 1);
+        const updatedUser = await User.updateOne(
+            { _id: userId },
+            { $push: { Projects: createdProject._id } }
+        );
 
-    const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-    const { username, _id: userId } = createdUsers.ops[randomUserIndex];
+        createdProjects.push(createdProject);
+    }
 
-    const createdProject = await Project.create({ title, description, link, username });
 
-    const updatedUser = await User.updateOne(
-      { _id: userId },
-      { $push: { Projects: createdProject._id } }
-    );
 
-    createdProjects.push(createdProject);
-  }
+    // create tags
+    const sampleTags = ['MERN', 'Node/Express', 'front-end'];
+    // have list of tags to choose from, following logic will then be changed to choose tags randomly and then apply them to the projects created above
 
-  // create tags
+    for (let i = 0; i < 100; i += 1) {
+        const name = sampleTags[Math.round(Math.random() * 2) + 1];
+        
+        const randomProjectIndex = Math.floor(Math.random() * createdProjects.length);
+        const { title }  = createdProjects[randomProjectIndex];
+        
+        
+console.log(await Project.findOne({title: title}))
+        console.log(title)
 
-  // need list of tags to choose from, following logic will then be changed to choose tags randomly and then apply them to the projects created above
+        await Project.updateOne(
+            { title:  title  },
+            { $addToSet: { tags: name  } },  { safe: true, upsert: true }, (err, data) => {
+                if (err){
+                    console.log(err);
+                }
+                console.log(data)
+                } 
+        );
+    }
 
-//   for (let i = 0; i < 100; i += 1) {
-//     const name = faker.lorem.words(Math.round(Math.random() * 20) + 1);
-
-//     const randomUserIndex = Math.floor(Math.random() * createdUsers.ops.length);
-//     const { username } = createdUsers.ops[randomUserIndex];
-
-//     const randomThoughtIndex = Math.floor(Math.random() * createdThoughts.length);
-//     const { _id: thoughtId } = createdThoughts[randomThoughtIndex];
-
-//     await Thought.updateOne(
-//       { _id: thoughtId },
-//       { $push: { reactions: { reactionBody, username } } },
-//       { runValidators: true }
-//     );
-//   }
-
-  console.log('all done!');
-  process.exit(0);
+    console.log('all done!');
+    process.exit(0);
 });
