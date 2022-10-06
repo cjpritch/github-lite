984661b0ba6e47892303044d96218ea50360537c
Amy Gardiner <amymgardiner@gmail.com>
2022-10-06 12:08:24 -0500

    imported new pages and components to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 import Home from './pages/Home';
 import Login from './pages/Login';
-import NoMatch from './pages/NoMatch';
 import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import SingleProject from './pages/SingleThought';
+import NoMatch from './pages/NoMatch';
+
 
 function App() {
   return (
