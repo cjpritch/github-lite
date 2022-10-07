900fc29b9cf631983a054d261c8dd07e364b5a49
Amy Gardiner <amymgardiner@gmail.com>
2022-10-06 20:54:41 -0500

    finished some more basic file structure for all components and pages

---
+++ client/src/components/ProjectList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProjectList = ({ projects, title }) => {
+  if (!projects.length) {
+    return <h3>No Projects Posted Yet</h3>;
+  }
+
+  return (
+  );
+};
+
+export default ProjectList;
