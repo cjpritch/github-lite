2a23d5937f094400e2d5c898e599b31425879a7d
Amy Gardiner <amymgardiner@gmail.com>
2022-10-07 15:23:59 -0500

    started on project form but not really sure yet what to do fully

--- client/src/components/ProjectForm/index.js
+++ client/src/components/ProjectForm/index.js
@@ -1,1 +1,10 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_PROJECT } from '../../utils/mutations';
+import { QUERY_PROJECTS, QUERY_ME } from '../../utils/queries';
+
+const ProjectForm = () => {
+    return ()
+}
+
+export default ProjectForm;
