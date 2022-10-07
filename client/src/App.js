78800ac4bc696d63896d13cc510bb3d0aae48895
Amy Gardiner <amymgardiner@gmail.com>
2022-10-07 14:54:00 -0500

    set up apollo client in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 // import Header from './components/Header';
 // import Footer from './components/Footer';
@@ -7,11 +16,38 @@ import React from 'react';
 // import Login from './pages/Login';
 // import Signup from './pages/Signup';
 // import Profile from './pages/Profile';
-// import SingleProject from './pages/SingleThought';
-// import NoMatch from './pages/NoMatch';
+// import SingleProject from './pages/SingleProject';
+import NoMatch from './pages/NoMatch';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  return <h1>hi.</h1>;
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes>
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
