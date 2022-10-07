9870fc7a7b0a2061f7581823d1c51f2ebbd1d82e
Amy Gardiner <amymgardiner@gmail.com>
2022-10-07 15:19:48 -0500

    finished basic apollo and graphql features for the pages

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import Auth from '../utils/auth';
 import ProjectForm from '../components/ProjectForm';
+import ProjectList from '../components/ProjectList';
 
 const Profile = () => {
-    return ();
+  const { username: userParam } = useParams();
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+  const user = data?.me || data?.user || {};
+
+  // navigate to personal profile page if username is the logged-in user's
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return ();
 }
 
 export default Profile;
